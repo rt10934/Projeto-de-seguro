b3a42c3d9ab5e82d9d272b75d5015f44045a1699
Ronaldo <torresronaldo@yahoo.com.br>
2023-08-04 23:45:25 -0300

    only simple change in incia_rgee_linux

--- Inicia_rgee_linux.R
+++ Inicia_rgee_linux.R
@@ -10,14 +10,3 @@ ee_Authenticate()
 ee_Initialize()
 rgee::ee_check()
 rgee::ee_check_python()
-
-
-
-
-If you encounter any issues during the process, carefully review the error messages and check if there are any missing dependencies or misconfigurations. The error message also suggests running ee_clean_pyenv() before retrying, which can be helpful if there are any remnants of a previously unsuccessful installation.
-
-If the problem persists or you encounter different errors, please provide the new error messages and additional information about your system setup, R version, Python version, and any recent changes you made. This information will aid in further troubleshooting.
-
-rgee_environment_dir = "/home/ronaldo/anaconda3/envs/rgee:/home/ronaldo/anaconda3/envs/rgee"
-Sys.setenv(RETICULATE_PYTHON = rgee_environment_dir)
-
